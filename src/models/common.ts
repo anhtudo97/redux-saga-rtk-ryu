a4a3547a6d9c16018dda0a66cf746c993b49e85b
Zhao Yang <anhtudo97@gmail.com>
2022-03-17 22:59:12 +0700

    feat(api): update

--- src/models/common.ts
+++ src/models/common.ts
@@ -8,3 +8,12 @@ export interface ListRespons<T> {
   data: T[];
   pagination: PaginationParams;
 }
+
+export interface ListParams {
+  _page?: number;
+  _limit?: number;
+  _sort?: string;
+  _order?: 'asc' | 'desc';
+
+  [key: string]: any;
+}
